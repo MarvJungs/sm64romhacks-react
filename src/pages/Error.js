d9206a74924a056815a9acb7cf72a5b2f82bcaaf
MarvJungs <Marvin10072000@live.de>
2024-01-23 01:26:00 +0100

    404 page

---
+++ src/pages/Error.js
@@ -0,0 +1,9 @@
+export default function Error() {
+    return (
+        <>
+            <h5>Error 404.</h5>
+            An oopsie happened :/
+            <a href="/" className="btn btn-primary">Return To Main</a>
+        </>
+    )
+}
